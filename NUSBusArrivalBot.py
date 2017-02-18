13aa3c6afc47efddfb6ee04c846d1ca36425ac95
Shao Hui <shinlos@hotmail.com>
2017-02-18 15:45:14 +0800

    add customisation options

--- NUSBusArrivalBot.py
+++ NUSBusArrivalBot.py
@@ -32,6 +32,15 @@ def getStopsList():
     return result["BusStopsResult"]
 
 
+#gets a dictionary that maps the stopID to its name
+def getStopsDict():
+    stopsDict = {}
+    for stop in stopsList:
+        stopsDict[stop["name"]] = stop["caption"]
+
+    return stopsDict
+
+
 #does the ajax call to the API
 def getArrivals(stopID):
     url = 'http://nextbus.comfortdelgro.com.sg/testMethod.asmx/GetShuttleService?busstopname=' + stopID
@@ -98,6 +107,9 @@ def getNearestStops(queryPoint):
     return nearestStops
 
 
+def getStopName(stopID):
+    return stopsDict[stopID]
+
 #bot functions
 
 #sends a welcome message to the user
@@ -108,12 +120,21 @@ def start(bot, update):
     #initialise custom keyboard
     keyboard = []
     sendLocationButton = KeyboardButton(text="Get nearest stops", request_location=True)
-    keyboard.append(["/getstops", sendLocationButton])
+    keyboard.append(["/customise", "/getstops", sendLocationButton])
 
-    reply_markup = ReplyKeyboardMarkup(keyboard)
+    reply_markup = ReplyKeyboardMarkup(keyboard=keyboard, one_time_keyboard=True)
 
     text = "Hello! This is a telegram bot for getting the arrival timings of NUS busses."
-    bot.sendMessage(chat_id, text, reply_markup=reply_markup)
+    bot.sendMessage(chat_id=chat_id, text=text, reply_markup=reply_markup)
+
+    #add user to database
+    payload = {
+        "chat_id": chat_id
+    }
+
+    db.user_settings.update_one(filter={'chat_id' : chat_id}, 
+                                update={'$set': payload}, 
+                                upsert=True)
 
 
 #uses the global variable stopsList
@@ -125,11 +146,10 @@ def getstops(bot, update):
     for stop in stopsList:
         buttonText = stop["caption"]
         data = {
-            "mode": "get_timing",
+            "user_state": "get_timing",
             "stopID": stop["name"]
         }
         payload = json.dumps(data)
-        buttonText = stop["caption"]
         keyboard.append([InlineKeyboardButton(buttonText, callback_data=payload)])
 
     reply_markup = InlineKeyboardMarkup(keyboard)
@@ -144,20 +164,20 @@ def location(bot, update):
     latitude = update.message.location.latitude
     longitude = update.message.location.longitude
 
-    queryPoint = (latitude, longitude)
+    query_point = (latitude, longitude)
 
     #get nearest stops
-    nearestStops = getNearestStops(queryPoint)
+    nearestStops = getNearestStops(query_point)
 
     #craft the inlinekeyboard buttons
     keyboard = []
     for stop in nearestStops:
         buttonText = stop["caption"]
         data = {
-            "mode": "get_timing",
+            "user_state": "get_timing",
             "stopID": stop["name"]
         }
-        payload = json.dumps(json)
+        payload = json.dumps(data)
         keyboard.append([InlineKeyboardButton(buttonText, callback_data=payload)])
     
     reply_markup = InlineKeyboardMarkup(keyboard)
@@ -173,15 +193,208 @@ def button(bot, update):
     message_id = query.message.message_id
 
     content = json.loads(query.data)
-    stopID = content["stopID"]
+    user_state = content["user_state"]
 
-    #Disable the keyboard while the information is being retrieved
-    text = "Loading...\n"
-    bot.editMessageText(chat_id=chat_id, text=text, message_id=message_id)
+    if user_state == "get_timing":
+        stop_id = content["stopID"]
 
-    text = getArrivalsText(stopID)
+        #Disable the keyboard while the information is being retrieved
+        text = "Loading...\n"
+        bot.editMessageText(chat_id=chat_id, text=text, message_id=message_id)
 
-    bot.editMessageText(chat_id=chat_id, text=text, message_id=message_id)
+        text = getArrivalsText(stop_id)
+
+        bot.editMessageText(chat_id=chat_id, text=text, message_id=message_id)
+
+
+    if user_state == "settings_add":
+        text = "Add stops"
+        bot.editMessageText(chat_id=chat_id, text=text, message_id=message_id)
+        showAddStops(chat_id)
+
+    if user_state == "add_new_stop":
+        stop_id = content["stopID"]
+        text = getStopName(stop_id) + " was added"
+        bot.editMessageText(chat_id=chat_id, text=text, message_id=message_id)
+        addStop(chat_id, stop_id)
+
+    if user_state == "add_new_stop_done":
+        text = "Done"
+        bot.editMessageText(chat_id=chat_id, text=text, message_id=message_id)
+        showSettings(chat_id)
+
+    if user_state == "settings_remove":
+        text = "Remove stops"
+        bot.editMessageText(chat_id=chat_id, text=text, message_id=message_id)
+        showRemoveStops(chat_id)
+
+    if user_state == "remove_stop":
+        stop_id = content["stopID"]
+        text = getStopName(stop_id) + " was removed"
+        bot.editMessageText(chat_id=chat_id, text=text, message_id=message_id)
+        removeStop(chat_id, stop_id)
+
+    if user_state == "remove_stop_done":
+        text = "Done"
+        bot.editMessageText(chat_id=chat_id, text=text, message_id=message_id)
+        showSettings(chat_id)
+
+    if user_state == "exit_settings":
+        text = "Changes have been saved"
+        bot.editMessageText(chat_id=chat_id, text=text, message_id=message_id)
+
+        #initialise user's custom keyboard
+        keyboard = []
+        custom_stops = getUserStopsList(chat_id)
+        for stop in custom_stops:
+            keyboard.append([getStopName(stop)])
+
+        reply_markup = ReplyKeyboardMarkup(keyboard=keyboard, one_time_keyboard=True)
+
+        text = "Hello! This is a telegram bot for getting the arrival timings of NUS busses."
+        bot.sendMessage(chat_id=chat_id, text=text, reply_markup=reply_markup)
+
+
+#allow the user to add in their own custom keyboard
+def settings(bot, update):
+    chat_id = update.message.chat_id
+    showSettings(chat_id)
+
+
+def showSettings(chat_id):
+    keyboard = []
+
+    data = {"user_state": "settings_add"}
+    payload = json.dumps(data)
+    keyboard.append([InlineKeyboardButton("Add", callback_data=payload)])
+    payload = json.dumps({"user_state": "settings_remove"})
+    keyboard.append([InlineKeyboardButton("Remove", callback_data=payload)])
+    # payload = json.dumps({"user_state": "settings_reorder"})
+    # keyboard.append([InlineKeyboardButton("Reorder", callback_data=payload)])
+    data = {"user_state": "exit_settings"}
+    payload = json.dumps(data)
+    keyboard.append([InlineKeyboardButton("Done", callback_data=payload)])
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    text = "Choose an action"
+
+    bot.sendMessage(chat_id=chat_id, text=text, reply_markup=reply_markup)
+
+
+#returns an array of the user custom stops
+def getUserStopsList(chat_id):
+    #retrieve user's current custom list from db
+    existing_data = db.user_settings.find_one({"chat_id": chat_id})
+    print(existing_data)
+
+    if "custom_stops" in existing_data:
+        #load into array
+        custom_stops = existing_data["custom_stops"]
+    else:
+        custom_stops = []
+
+    return custom_stops
+
+
+#current user state must be "settings_add"
+def showAddStops(chat_id):
+    #get user's current custom list of stops
+    custom_stops = getUserStopsList(chat_id)
+
+    #craft inline keyboard
+    keyboard = []
+
+    for stop in stopsList:
+        if stop["name"] not in custom_stops:
+            buttonText = stop["caption"]
+            data = {
+                "user_state": "add_new_stop",
+                "stopID": stop["name"]
+            }
+            payload = json.dumps(data)
+            keyboard.append([InlineKeyboardButton(buttonText, callback_data=payload)])
+
+    #add a done button
+    #brings the user back to settings
+    data = {
+            "user_state": "add_new_stop_done"
+        }
+    payload = json.dumps(data)
+    keyboard.append([InlineKeyboardButton("Done", callback_data=payload)])
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    text = "Pick a stop:"
+
+    bot.sendMessage(chat_id=chat_id, text=text, reply_markup=reply_markup)
+
+
+#current user state must be "settings_add_new_stop"
+#brings user back to showAddStops
+def addStop(chat_id, stop_id):
+    custom_stops = getUserStopsList(chat_id)
+    custom_stops.append(stop_id)
+
+    payload = {
+        "custom_stops": custom_stops
+    }
+
+    #update user_state and custom_stops in database
+    db.user_settings.update_one(filter={'chat_id' : chat_id},
+                                update={'$set': payload},
+                                upsert=True)
+
+    showAddStops(chat_id)
+
+
+def showRemoveStops(chat_id):
+    #get user's current custom list of stops
+    custom_stops = getUserStopsList(chat_id)
+    print(custom_stops)
+
+    #craft the keyboard
+    keyboard = []
+
+    for stop in custom_stops:
+        buttonText = getStopName(stop)
+        data = {
+            "user_state": "remove_stop",
+            "stopID": stop
+        }
+        payload = json.dumps(data)
+        keyboard.append([InlineKeyboardButton(buttonText, callback_data=payload)])
+
+    #add a done button
+    #brings the user back to settings
+    data = {
+            "user_state": "remove_stop_done"
+        }
+    payload = json.dumps(data)
+    keyboard.append([InlineKeyboardButton("Done", callback_data=payload)])
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    text = "Pick a stop to remove:"
+
+    bot.sendMessage(chat_id=chat_id, text=text, reply_markup=reply_markup)
+
+
+def removeStop(chat_id, stop_id):
+    custom_stops = getUserStopsList(chat_id)
+    custom_stops.remove(stop_id)
+
+    payload = {
+        "custom_stops": custom_stops
+    }
+
+    #update user_state and custom_stops in database
+    db.user_settings.update_one(filter={'chat_id' : chat_id},
+                                update={'$set': payload},
+                                upsert=True)
+
+    showRemoveStops(chat_id)
+
+
+def reorderStops(chat_id):
+    pass
 
 
 def help(bot, update):
@@ -201,6 +414,8 @@ def main():
     global stopsList #use global variable so it only needs to be initialised once
     stopsList = getStopsList()
     stopsList = stopsList["busstops"]
+    global stopsDict
+    stopsDict = getStopsDict()
 
     # initialise database
     client = MongoClient(MLABS_DATABASE)
@@ -215,18 +430,19 @@ def main():
     bot = Bot(token=TOKEN)
 
     # setup webhook
-    PORT = int(environ.get('PORT', '5000'))
-    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-    updater.bot.setWebhook(APP_URL + TOKEN)
+    # PORT = int(environ.get('PORT', '5000'))
+    # updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+    # updater.bot.setWebhook(APP_URL + TOKEN)
 
-    # # Use long polling (disabled when webhooks are enabled)
-    # updater.start_polling()
+    # Use long polling (disabled when webhooks are enabled)
+    updater.start_polling()
 
     # add handlers
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('getstops', getstops))
     updater.dispatcher.add_handler(MessageHandler(Filters.location, location))
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_handler(CommandHandler('customise', settings))
     updater.dispatcher.add_handler(CommandHandler('help', help))
     updater.dispatcher.add_error_handler(error)
 
