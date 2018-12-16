86a8de83326a8616ec43c8c845630e92fe3a5b8b
Shao Hui <shinlos@hotmail.com>
2018-12-17 00:26:31 +0800

    add class for BusStopLocation and BusStopLocationList

---
+++ bus_stop_location.py
@@ -0,0 +1,6 @@
+class BusStopLocation:
+    def __init__(self, name, caption, latitude, longitude):
+        self.name = name
+        self.caption = caption
+        self.latitude = 0
+        self.longitude = 0
