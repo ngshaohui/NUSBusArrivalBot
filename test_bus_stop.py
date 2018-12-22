2618705af310b92c79ac02742c7f58e8cddf28f4
Shao Hui <shinlos@hotmail.com>
2018-12-22 22:48:32 +0800

    refactor code

--- test_bus_stop.py
+++ test_bus_stop.py
@@ -1,23 +1,29 @@
 import json
 
 from bus_stop import BusStop
+from bus_stop_list import BusStopList
 
-# TODO should have more test date to load
+# TODO more tests
 
 
 class TestBusStop:
 
     # all newly initialised BusStop should be invalid
-    def test_validity(self):
-        with open("./test/bus_stop_list.json") as f:
+    def testValidity(self):
+        # BusStopList validity
+        bus_stops = BusStopList()
+        assert bus_stops.valid is False
+
+        # BusStop validity
+        with open("./test_data/bus_stop_list.json") as f:
             data = json.load(f)
-        bus_stops = data["BusStopsResult"]["busstops"]
+        bus_stops_data = data["BusStopsResult"]["busstops"]
         bus_stop_list = []
-        for bus_stop in bus_stops:
-                service = BusStop(bus_stop["name"],
-                                  bus_stop["caption"],
-                                  bus_stop["latitude"],
-                                  bus_stop["longitude"])
-                bus_stop_list.append(service)
+        for bus_stop in bus_stops_data:
+            service = BusStop(bus_stop["name"],
+                              bus_stop["caption"],
+                              bus_stop["latitude"],
+                              bus_stop["longitude"])
+            bus_stop_list.append(service)
         for bus_stop in bus_stop_list:
             assert bus_stop.valid is False
